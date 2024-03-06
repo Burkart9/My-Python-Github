4d05f7b2a9ac58cd48f243649a0bef7f7ba8d4ff
Burkart9 <133555444+Burkart9@users.noreply.github.com>
2024-03-06 13:42:38 +0800

    更新 lol_auto.py
    优化代码

--- lol_auto.py
+++ lol_auto.py
@@ -5,15 +5,12 @@ import pyautogui
 # Functions
 def auto_click(t:int):
     time.sleep(t)
-    print("Start!")
+    print("开始!")
     pyautogui.rightClick(1900,900)
     print("右键点击一次")
     time.sleep(0.1)
     pyautogui.leftClick(1900,900)
     print("左键点击一次")
-
-
-
 
 # Main
 if __name__=="__main__":
